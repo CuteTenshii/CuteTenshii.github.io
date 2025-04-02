df8ccb59c74734ae4e3825a2c90662760df8549e
Yuuto <55290516+JustYuuto@users.noreply.github.com>
2025-04-02 16:15:43 +0200

    fix: disable images optimization

--- next.config.ts
+++ next.config.ts
@@ -4,13 +4,7 @@ import createNextIntlPlugin from 'next-intl/plugin';
 const nextConfig: NextConfig = {
   poweredByHeader: false,
   images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'raw.githubusercontent.com',
-        pathname: '/JustYuuto/deezer-discord-rpc/master/screenshots/rpc.png',
-      }
-    ]
+    unoptimized: true,
   },
   output: 'export',
 };
